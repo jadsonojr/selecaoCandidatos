668f5d82cd88341d68744ab735911a01b3f9f1cb
jadsonojr <jadson.personal.edf@gmail.com>
2024-06-29 23:09:26 -0300

    inicio do projeto

---
+++ src/selecaoCandidatos.java
@@ -0,0 +1,17 @@
+public class selecaoCandidatos {
+    public static void main(String[] args) throws Exception {
+    System.out.println("PROCESSO SELETIVO");
+
+    }
+    static void analisarCandidato (double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido)
+            System.out.println("ligar para candidato");
+        
+    } else if  ( "LIGAR PARA CADIDATO E FAZER CONTRA PROPOSTA ");
+        else{
+            System.out.println("aguardadno o resultado dos demais candidatos");
+        }
+    }
+
+
